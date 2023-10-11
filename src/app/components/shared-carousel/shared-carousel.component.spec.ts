c9bca2fd4e1bfd301360c5c9cc7dfdc13d3455ba
rajeevGaur <73661588+rajeevGaur@users.noreply.github.com>
2023-10-11 23:11:46 +0530

    added shared carousel for owl carousel image

---
+++ src/app/components/shared-carousel/shared-carousel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SharedCarouselComponent } from './shared-carousel.component';
+
+describe('SharedCarouselComponent', () => {
+  let component: SharedCarouselComponent;
+  let fixture: ComponentFixture<SharedCarouselComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SharedCarouselComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SharedCarouselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
