5d5a61b7bd482f7cd368024ae93fa757ffab135c
rajeevGaur <73661588+rajeevGaur@users.noreply.github.com>
2023-10-11 15:01:39 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomePageComponent} from './components/homePageComponent/homePageComponent';
+import {HomePageComponent} from './components/homePageComponent/HomePageComponentComponent';
 const routes: Routes = [
       {
         path: 'home', component: HomePageComponent
