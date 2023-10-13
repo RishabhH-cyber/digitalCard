4b35a0fa34df947f81831c04076de2c78e68d981
nikita2995 <khandelwalnikita02@gmail.com>
2023-10-13 19:57:27 +0530

    Added names services function
    Added names services function

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -8,9 +8,16 @@ export class UtilsService {
   private usernameSource = new BehaviorSubject<string>(''); // Initial value is an empty string
   currentUsername = this.usernameSource.asObservable();
 
+  private nameSource = new BehaviorSubject<string>(''); // Initial value is an empty string
+  currentName = this.nameSource.asObservable();
+
   constructor() { }
 
   updateUsername(newUsername: string) {
     this.usernameSource.next(newUsername);
+  }
+
+  updateName(newName: string) {
+    this.nameSource.next(newName);
   }
 }
